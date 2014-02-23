3f2c9e369512f553d79d785030c30a6d3cc697a9
gwatson <gwatson@gmail.com>
2014-02-23 14:58:59 -0800

    Added module_parse_tree to Compiler object to map module names to their original PyParsing parse trees so that when a module is instantiated then the compiler just recompiles the code.

--- VeriParser/VeriCompile.py
+++ VeriParser/VeriCompile.py
@@ -2,14 +2,14 @@
 # Given a parse tree, compile it.
 
 import Global, VeriModule, VeriTime
-import sys
+import sys, copy
 
 
 class Compiler(object):
 
     def __init__(self):
 
-        self.module_parse_tree = {}  # dict maping module name to modules parse tree.
+        self.module_parse_tree = {}  # dict mapping module name to that module's parse tree.
         self.timescale         = VeriTime.TimeScale() # current timescale
 
 
@@ -21,8 +21,16 @@ class Compiler(object):
         '''
 
         for el in parse_tree:
-            # print "<", el, ">"
+            #print "<", el, ">"
+
             if el[0] == 'module_decl':
+                
+                # Copy the parse tree and associate it with the module name.
+                assert el[1][0] == 'module_name'
+                mod_name  = el[1][1]
+                print "Compiler: found source definition for module", mod_name
+                self.module_parse_tree[mod_name] = el.copy()
+
                 m = VeriModule.VeriModule( self.timescale)
                 m.process_element(gbl, 0, el)
                 if gbl.debug:
